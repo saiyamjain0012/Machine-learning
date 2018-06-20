43758498746e1f55a87ed125eccbfe392fc21762
saiyamjain0012 <saiyamjain0012@gmail.com>
2018-06-20 23:06:45 +0530

    Add files via upload

---
+++ Handwritten_number_Detection.py
@@ -0,0 +1,231 @@
+import cv2
+import dlib
+import numpy as np
+
+
+PREDICTOR_PATH = "shape_predictor_68_face_landmarks.dat"
+predictor = dlib.shape_predictor(PREDICTOR_PATH)
+#cascade_path='haarcascade_frontalface_default.xml'
+#cascade = cv2.CascadeClassifier(cascade_path)
+detector = dlib.get_frontal_face_detector()
+
+
+def get_landmarks(im):
+    rects = detector(im, 1)
+
+    if len(rects) > 1:
+        return "error"
+    if len(rects) == 0:
+        return "error"
+    return np.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+
+
+def annotate_landmarks(im, landmarks):
+    im = im.copy()
+    for idx, point in enumerate(landmarks):
+        pos = (point[0, 0], point[0, 1])
+        cv2.putText(im, str(idx), pos,
+                    fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+                    fontScale=0.4,
+                    color=(0, 0, 255))
+        cv2.circle(im, pos, 3, color=(0, 255, 255))
+    return im
+
+def top_lip(landmarks):
+    top_lip_pts = []
+    for i in range(50,53):
+        top_lip_pts.append(landmarks[i])
+    for i in range(61,64):
+        top_lip_pts.append(landmarks[i])
+    top_lip_all_pts = np.squeeze(np.asarray(top_lip_pts))
+    top_lip_mean = np.mean(top_lip_pts, axis=0)
+    return int(top_lip_mean[:,1])
+
+def bottom_lip(landmarks):
+    bottom_lip_pts = []
+    for i in range(65,68):
+        bottom_lip_pts.append(landmarks[i])
+    for i in range(56,59):
+        bottom_lip_pts.append(landmarks[i])
+    bottom_lip_all_pts = np.squeeze(np.asarray(bottom_lip_pts))
+    bottom_lip_mean = np.mean(bottom_lip_pts, axis=0)
+    return int(bottom_lip_mean[:,1])
+
+def mouth_open(image):
+    landmarks = get_landmarks(image)
+    
+    if landmarks == "error":
+        return image, 0
+    
+    image_with_landmarks = annotate_landmarks(image, landmarks)
+    top_lip_center = top_lip(landmarks)
+    bottom_lip_center = bottom_lip(landmarks)
+    lip_distance = abs(top_lip_center - bottom_lip_center)
+    return image_with_landmarks, lip_distance
+
+    #cv2.imshow('Result', image_with_landmarks)
+    #cv2.imwrite('image_with_landmarks.jpg',image_with_landmarks)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+cap = cv2.VideoCapture(0)
+yawns = 0
+yawn_status = False 
+
+while True:
+    ret, frame = cap.read()   
+    image_landmarks, lip_distance = mouth_open(frame)
+    
+    prev_yawn_status = yawn_status  
+    
+    if lip_distance > 25:
+        yawn_status = True 
+        
+        cv2.putText(frame, "Subject is Yawning", (50,450), 
+                    cv2.FONT_HERSHEY_COMPLEX, 1,(0,0,255),2)
+        
+
+        output_text = " Yawn Count: " + str(yawns + 1)
+
+        cv2.putText(frame, output_text, (50,50),
+                    cv2.FONT_HERSHEY_COMPLEX, 1,(0,255,127),2)
+        
+    else:
+        yawn_status = False 
+         
+    if prev_yawn_status == True and yawn_status == False:
+        yawns += 1
+
+    cv2.imshow('Live Landmarks', image_landmarks )
+    cv2.imshow('Yawn Detection', frame )
+    
+    if cv2.waitKey(1) == 13: #13 is the Enter Key
+        break
+        
+cap.release()
+cv2.destroyAllWindows() 
+import numpy as np
+import cv2
+
+# Define our functions
+
+def x_cord_contour(contour):
+    # This function take a contour from findContours
+    # it then outputs the x centroid coordinates
+    
+    if cv2.contourArea(contour) > 10:
+        M = cv2.moments(contour)
+        return (int(M['m10']/M['m00']))
+
+def makeSquare(not_square):
+    # This function takes an image and makes the dimenions square
+    # It adds black pixels as the padding where needed
+    
+    BLACK = [0,0,0]
+    img_dim = not_square.shape
+    height = img_dim[0]
+    width = img_dim[1]
+    #print("Height = ", height, "Width = ", width)
+    if (height == width):
+        square = not_square
+        return square
+    else:
+        doublesize = cv2.resize(not_square,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+        height = height * 2
+        width = width * 2
+        #print("New Height = ", height, "New Width = ", width)
+        if (height > width):
+            pad = (height - width)/2
+            #print("Padding = ", pad)
+            doublesize_square = cv2.copyMakeBorder(doublesize,0,0,pad,\
+                                                   pad,cv2.BORDER_CONSTANT,value=BLACK)
+        else:
+            pad = (width - height)/2
+            #print("Padding = ", pad)
+            doublesize_square = cv2.copyMakeBorder(doublesize,pad,pad,0,0,\
+                                                   cv2.BORDER_CONSTANT,value=BLACK)
+    doublesize_square_dim = doublesize_square.shape
+    #print("Sq Height = ", doublesize_square_dim[0], "Sq Width = ", doublesize_square_dim[1])
+    return doublesize_square
+
+
+def resize_to_pixel(dimensions, image):
+    # This function then re-sizes an image to the specificied dimenions
+    
+    buffer_pix = 4
+    dimensions  = dimensions - buffer_pix
+    squared = image
+    r = float(dimensions) / squared.shape[1]
+    dim = (dimensions, int(squared.shape[0] * r))
+    resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+    img_dim2 = resized.shape
+    height_r = img_dim2[0]
+    width_r = img_dim2[1]
+    BLACK = [0,0,0]
+    if (height_r > width_r):
+        resized = cv2.copyMakeBorder(resized,0,0,0,1,cv2.BORDER_CONSTANT,value=BLACK)
+    if (height_r < width_r):
+        resized = cv2.copyMakeBorder(resized,1,0,0,0,cv2.BORDER_CONSTANT,value=BLACK)
+    p = 2
+    ReSizedImg = cv2.copyMakeBorder(resized,p,p,p,p,cv2.BORDER_CONSTANT,value=BLACK)
+    img_dim = ReSizedImg.shape
+    height = img_dim[0]
+    width = img_dim[1]
+    #print("Padded Height = ", height, "Width = ", width)
+    return ReSizedImg
+import numpy as np
+import cv2
+
+image = cv2.imread('images/numbers.jpg')
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+cv2.imshow("image", image)
+cv2.imshow("gray", gray)
+cv2.waitKey(0)
+
+# Blur image then find edges using Canny 
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+cv2.imshow("blurred", blurred)
+cv2.waitKey(0)
+
+edged = cv2.Canny(blurred, 30, 150)
+cv2.imshow("edged", edged)
+cv2.waitKey(0)
+
+# Fint Contours
+contours, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+#Sort out contours left to right by using their x cordinates
+contours = sorted(contours, key = x_cord_contour, reverse = False)
+
+# Create empty array to store entire number
+full_number = []
+
+# loop over the contours
+for c in contours:
+    # compute the bounding box for the rectangle
+    (x, y, w, h) = cv2.boundingRect(c)    
+    
+    #cv2.drawContours(image, contours, -1, (0,255,0), 3)
+    #cv2.imshow("Contours", image)
+
+    if w >= 5 and h >= 25:
+        roi = blurred[y:y + h, x:x + w]
+        ret, roi = cv2.threshold(roi, 127, 255,cv2.THRESH_BINARY_INV)
+        squared = makeSquare(roi)
+        final = resize_to_pixel(20, squared)
+        cv2.imshow("final", final)
+        final_array = final.reshape((1,400))
+        final_array = final_array.astype(np.float32)
+        ret, result, neighbours, dist = knn.find_nearest(final_array, k=1)
+        number = str(int(float(result[0])))
+        full_number.append(number)
+        # draw a rectangle around the digit, the show what the
+        # digit was classified as
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.putText(image, number, (x , y + 155),
+            cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 0), 2)
+        cv2.imshow("image", image)
+        cv2.waitKey(0) 
+        
+cv2.destroyAllWindows()
+print ("The number is: " + ''.join(full_number))
